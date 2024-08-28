bfe54a295851155fa43604de6791af3c176d610f
ho5h1 <146405471+ho5h1@users.noreply.github.com>
2024-08-28 09:01:30 +0800

    fix singleplayer

--- src/main/java/dev/o7moon/openboatutils/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/dev/o7moon/openboatutils/mixin/ServerPlayNetworkHandlerMixin.java
@@ -5,11 +5,17 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public class ServerPlayNetworkHandlerMixin {
     @Inject(method = "isMovementInvalid", at = @At("HEAD"), cancellable = true)
     private static void isMovementInvalid(CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(false);// !!! this disables the movement anti-cheat, but is necessary for stepping to work in singleplayer
+    }
+    @ModifyConstant(method = "onVehicleMove", constant = @Constant(doubleValue = 0.0625d))
+    private double movedWronglyThreshold(double original) {
+        return 16d;
     }
 }
